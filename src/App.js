5186f671050936a643522ff0f63c098e0f05e17f
Ron Salomon <ron.salomon@hotmail.com>
2021-10-22 01:41:55 -0400

    changes for phase 1 of app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+      params={particlesOptions}
+      />
       <Navigation />
-        {/*   <Logo />
-            <ImageFromLink />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
             <FaceRecognition /> 
         */}
     </div>
