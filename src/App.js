c0fcea9bf6d17acfba88c95c861f3cc5a283aeec
Ron Salomon <ron.salomon@hotmail.com>
2021-10-25 22:18:26 -0400

    completed facedetection app code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import Particles from "react-tsparticles";
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
+import Particles from 'react-tsparticles';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+// api for Clarifai.com
+const app = new Clarifai.App({
+  apiKey: '88d85d28b1ce415ba1ca69d563f5d31c'
+});
+
 
 const particlesOptions = {
   particles: {
@@ -18,21 +26,66 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-      params={particlesOptions}
-      />
-      <Navigation />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL:'',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input})
+
+    app.models.predict(
+        Clarifai.FACE_DETECT_MODEL, this.state.input
+        )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+        params={particlesOptions}
+        />
+        <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*
-            <FaceRecognition /> 
-        */}
-    </div>
-  );
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
